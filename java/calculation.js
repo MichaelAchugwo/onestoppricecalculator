c251ba90d6ebcbaee13a5ef2a61a3996411aeac0
Achugwo Michael <139189514+MichaelAchugwo@users.noreply.github.com>
2025-02-24 18:36:38 +0100

    added support for calculating both paper print and large format print

--- java/calculation.js
+++ java/calculation.js
@@ -1,50 +1,112 @@
-var showWeb;
-var mainCalculator = document.getElementById("mainCalculator");
+var paperPrintBox = document.getElementById("paperPrintBox");
+var largeFormatBox = document.getElementById("largeFormatBox");
 
-function webLoader() {
-  showWeb = setTimeout(showPage, 1500);
+function toggleBoxes() {
+  const selectedOption = document.querySelector(
+    'input[name="printType"]:checked'
+  ).value;
+  if (selectedOption === "Paper Print") {
+    paperPrintBox.style.display = "block";
+    largeFormatBox.style.display = "none";
+  } else {
+    paperPrintBox.style.display = "none";
+    largeFormatBox.style.display = "block";
+  }
 }
 
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myWeb").style.display = "block";
-}
-
-var pricePerSqrFeet = 180;
+var newPricePerSqrFeet = 200;
+var a4Price = 200;
+var a3Price = 400;
 
 var select = document.getElementById("materialSelect");
-
-let previousOption;
-
-let selectedOptionValue = "option1";
+const selectPaper = document.getElementById("paperSelect");
 
 function updateVariable() {
-  const select = document.getElementById("materialSelect");
   const selectedOption = select.selectedIndex;
-  let newPricePerSqrFeet;
-  if (selectedOption === 0) {
-    return newPricePerSqrFeet;
-  } else if (selectedOption === 1) {
-    newPricePerSqrFeet = 200;
-  } else if (selectedOption === 2) {
-    newPricePerSqrFeet = 350;
-  } else if (selectedOption === 3) {
-    newPricePerSqrFeet = 350;
-  } else if (selectedOption === 4) {
-    newPricePerSqrFeet = 350;
-  } else if (selectedOption === 5) {
-    newPricePerSqrFeet = 350;
-  } else if (selectedOption === 6) {
-    newPricePerSqrFeet = 350;
+  const selectedPaper = selectPaper.selectedIndex;
+  switch (selectedOption) {
+    case 0:
+      newPricePerSqrFeet = 200;
+      break;
+    case 1:
+      newPricePerSqrFeet = 250;
+      break;
+    case 2:
+      newPricePerSqrFeet = 350;
+      break;
+    case 3:
+      newPricePerSqrFeet = 270;
+      break;
+    case 4:
+      newPricePerSqrFeet = 550;
+      break;
+    case 5:
+      newPricePerSqrFeet = 350;
+      break;
+    case 6:
+      newPricePerSqrFeet = 450;
+      break;
+    default:
+      newPricePerSqrFeet = 200;
   }
-  pricePerSqrFeet = newPricePerSqrFeet;
-  document.getElementById("price").innerHTML = pricePerSqrFeet;
-  document.getElementById("price2").innerHTML = pricePerSqrFeet;
+  switch (selectedPaper) {
+    case 0:
+      a4Price = 250;
+      a3Price = 450;
+      document.getElementById("a3Box").style.display = "inline-block";
+      break;
+    case 1:
+      a4Price = 100;
+      a3Price = 200;
+      document.getElementById("a3Box").style.display = "inline-block";
+      break;
+    case 2:
+      a4Price = 100;
+      a3Price = 200;
+      document.getElementById("a3Box").style.display = "inline-block";
+      break;
+    case 3:
+      a4Price = 200;
+      document.getElementById("a3Box").style.display = "none";
+      break;
+    case 4:
+      a4Price = 200;
+      document.getElementById("a3Box").style.display = "none";
+      break;
+  }
+  document.getElementById("price").innerHTML = newPricePerSqrFeet;
+  document.getElementById("price2").innerHTML = newPricePerSqrFeet;
   document.getElementById("calculated-price-in").innerHTML = "";
   document.getElementById("calculated-price-ft").innerHTML = "";
+  document.getElementById("a4price").innerHTML = a4Price;
+  document.getElementById("a3price").innerHTML = a3Price;
+  document.getElementById("calculated-price-a4").innerHTML = "";
+  document.getElementById("calculated-price-a3").innerHTML = "";
 }
 
-select.addEventListener("change", updateVariable);
+document.getElementById("a3number").addEventListener("input", function () {
+  var a3number = document.getElementById("a3number").value;
+  if (a3number >= 20 && selectedPaper === 0) {
+    a3Price = 400;
+  } else {
+    a3Price = 450;
+  }
+  document.getElementById("a3price").innerHTML = a3Price;
+});
+
+document.getElementById("a4number").addEventListener("input", function () {
+  var a4number = document.getElementById("a4number").value;
+  if (a4number >= 20 && selectedPaper === 0) {
+    a4Price = 200;
+  } else {
+    a4Price = 250;
+  }
+  document.getElementById("a4price").innerHTML = a4Price;
+});
+
+function formatNumber(num) {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 function calculatePriceIn() {
   var inwidth = document.getElementById("job-widthin").value;
@@ -52,20 +114,31 @@ function calculatePriceIn() {
   var innumber = document.getElementById("innumber").value;
   var inprice = document.getElementById("calculated-price-in");
   var inprice = Math.round(
-    (inwidth * inheight * innumber * pricePerSqrFeet) / 144
+    (inwidth * inheight * innumber * newPricePerSqrFeet) / 144
   );
 
-  document.getElementById("calculated-price-in").innerHTML = inprice;
+  document.getElementById("calculated-price-in").innerHTML = formatNumber(inprice);
 }
 function calculatePriceFt() {
   var ftwidth = document.getElementById("job-widthft").value;
   var ftheight = document.getElementById("job-heightft").value;
   var ftnumber = document.getElementById("ftnumber").value;
   var ftprice = document.getElementById("calculated-price-ft");
-  var ftprice = Math.round(ftwidth * ftheight * ftnumber * pricePerSqrFeet);
+  var ftprice = Math.round(ftwidth * ftheight * ftnumber * newPricePerSqrFeet);
 
-  document.getElementById("calculated-price-ft").innerHTML = ftprice;
+  document.getElementById("calculated-price-ft").innerHTML = formatNumber(ftprice);
 }
+function calculatePriceA4() {
+  var a4number = document.getElementById("a4number").value;
+  var a4price = a4Price * a4number;
+  document.getElementById("calculated-price-a4").innerHTML = formatNumber(a4price);
+}
+function calculatePriceA3() {
+  var a3number = document.getElementById("a3number").value;
+  var a4price = a3Price * a3number;
+  document.getElementById("calculated-price-a3").innerHTML = formatNumber(a4price);
+}
+
 function resetUnitsIn() {
   document.getElementById("job-widthin").value = "";
   document.getElementById("job-heightin").value = "";
@@ -78,6 +151,30 @@ function resetUnitsFt() {
   document.getElementById("ftnumber").value = "1";
   document.getElementById("calculated-price-ft").innerHTML = "";
 }
+function resetUnitsA4() {
+  document.getElementById("a4number").value = "1";
+  document.getElementById("calculated-price-a4").innerHTML = "";
+}
+function resetUnitsA3() {
+  document.getElementById("a3number").value = "1";
+  document.getElementById("calculated-price-a3").innerHTML = "";
+}
 
-document.getElementById("price").innerHTML = pricePerSqrFeet;
-document.getElementById("price2").innerHTML = pricePerSqrFeet;
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  updateVariable();
+
+  document.querySelectorAll('input[name="printType"]').forEach((elem) => {
+    elem.addEventListener("change", toggleBoxes);
+  });
+  
+  toggleBoxes();
+  select.addEventListener("change", updateVariable);
+  selectPaper.addEventListener("change", updateVariable);
+
+  document.getElementById("price").innerHTML = newPricePerSqrFeet;
+  document.getElementById("price2").innerHTML = newPricePerSqrFeet;
+  document.getElementById("a4price").innerHTML = a4Price;
+  document.getElementById("a3price").innerHTML = a3Price;
+});
